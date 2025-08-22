8abcf94ac70704011aaad71b4a739fb8a990af84
ethicode <papesow.digital@gmail.com>
2025-08-22 12:56:35 +0000

    update name

--- src/components/googledrivefilelist.js
+++ src/components/googledrivefilelist.js
@@ -4,6 +4,7 @@ import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
 import { useParams } from 'react-router-dom';
 import BottomPlayer from './bottomPlayer';
+import WaveSurfer from 'wavesurfer.js';
 
 const GoogleDriveFileList = () => {
     const [files, setFiles] = useState([]);
@@ -13,7 +14,9 @@ const GoogleDriveFileList = () => {
     const [loadingAudioId, setLoadingAudioId] = useState(null); // Id du fichier en cours de chargement
     const [audioProgress, setAudioProgress] = useState(0); // Pour suivre la progression de l'audio
     const [isPlaying, setIsPlaying] = useState(false); // Pour savoir si l'audio est en cours de lecture
-    const canvasRef = useRef(null); // Référence pour le canvas
+    const [waveform, setWaveform] = useState(null); // Référence du spectre
+    const waveformRef = useRef(null); // Référence de l'élément HTML pour le spectre
+
     const apiKey = 'AIzaSyA-JKB6f93YcyDFgz0KRuOuX9hWSHeFb5I';
     const { folderId } = useParams();
 
@@ -64,29 +67,27 @@ const GoogleDriveFileList = () => {
             setIsPlaying(false);
         });
 
-        // Création du contexte audio et de l'AnalyserNode
-        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        const analyser = audioContext.createAnalyser();
-        analyser.fftSize = 256; // Définir la taille de la FFT (Fast Fourier Transform) pour analyser les fréquences
-        const bufferSource = audioContext.createBufferSource();
-        
-        fetch(fileUrl)
-            .then(response => response.arrayBuffer())
-            .then(data => audioContext.decodeAudioData(data))
-            .then(decodedData => {
-                bufferSource.buffer = decodedData;
-                bufferSource.connect(analyser);
-                analyser.connect(audioContext.destination); // Connexion à la destination audio pour la lecture
-                bufferSource.start(0);
-                setAudio(newAudio);
-                setPlayingId(fileId); // L'audio commence à jouer
-                setIsPlaying(true); // L'audio est en cours de lecture
+        // Ajouter le spectre audio avec Wavesurfer
+        if (waveform) {
+            waveform.destroy(); // Détruire l'ancien spectre si un nouveau fichier est joué
+        }
 
-                // Démarrer l'animation du spectre
-                drawSpectre(analyser);
-            });
+        const newWaveform = WaveSurfer.create({
+            container: waveformRef.current,
+            waveColor: 'teal',
+            progressColor: 'darkgreen',
+            height: 100,
+            responsive: true,
+        });
 
-        setLoadingAudioId(fileId); // L'audio est en train de se charger, on affiche le spinner
+        newWaveform.load(fileUrl); // Charger l'audio dans le spectre
+
+        setWaveform(newWaveform);
+        newAudio.play();
+
+        setAudio(newAudio);
+        setPlayingId(fileId); // L'audio commence à jouer
+        setIsPlaying(true); // L'audio est en cours de lecture
     };
 
     const handlePauseAudio = () => {
@@ -94,44 +95,6 @@ const GoogleDriveFileList = () => {
             audio.pause();
             setIsPlaying(false); // Marquer comme non joué
         }
-    };
-
-    const drawSpectre = (analyser) => {
-        const canvas = canvasRef.current;
-        const canvasContext = canvas.getContext('2d');
-        const bufferLength = analyser.frequencyBinCount; // Nombre de fréquences analysées
-        const dataArray = new Uint8Array(bufferLength); // Tableau des données des fréquences
-
-        const WIDTH = canvas.width;
-        const HEIGHT = canvas.height;
-
-        // Fonction pour dessiner le spectre
-        const renderFrame = () => {
-            analyser.getByteFrequencyData(dataArray); // Récupérer les données des fréquences
-            canvasContext.fillStyle = 'rgba(0, 0, 0, 0.1)';
-            canvasContext.fillRect(0, 0, WIDTH, HEIGHT); // Effacer l'ancien spectre
-
-            const barWidth = WIDTH / bufferLength;
-            let x = 0;
-
-            // Dessiner chaque barre du spectre
-            for (let i = 0; i < bufferLength; i++) {
-                const barHeight = dataArray[i];
-
-                const red = barHeight + (25 * (i / bufferLength));
-                const green = 250 * (i / bufferLength);
-                const blue = 50;
-
-                canvasContext.fillStyle = `rgb(${red},${green},${blue})`;
-                canvasContext.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-
-                x += barWidth + 1;
-            }
-
-            requestAnimationFrame(renderFrame); // Demander à animer le prochain frame
-        };
-
-        renderFrame(); // Démarrer l'animation
     };
 
     return (
@@ -170,9 +133,9 @@ const GoogleDriveFileList = () => {
                 </Card>
             )}
 
-            {/* Spectre audio animé */}
+            {/* Spectre audio */}
             <Box sx={{ marginBottom: 2 }}>
-                <canvas ref={canvasRef} width="600" height="100" style={{ width: '100%' }}></canvas>
+                <div ref={waveformRef} style={{ width: '100%', height: '100px' }}></div>
             </Box>
 
             {/* Liste des fichiers */}
